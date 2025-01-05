f500f6365a40ef0cb52784c225be33aa0ff02ef9
mooshi_1 <achet92@gmail.com>
2025-01-05 17:47:14 -0500

    leaf/parent

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,16 +21,35 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+#leaf node doesn't accept all params from parent class
+#it must have values for tag and value, children is empty (must be none), and props are    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return self.value
+        convert = self.props_to_html()
+        return f"<{self.tag}{convert}>{self.value}</{self.tag}>"
+
+class ParentNode(HTMLNode):
+    def __init__():        
+        pass
+
 
 
 #this block only runs if the file is run directly, add these for testing purposes
 if __name__ == '__main__':
-    test_node = HTMLNode(
+    test_node = LeafNode(
         tag="a",
+        value='click me!',
         props={
             "href": "https://google.com",
             "class": "link-button"
         }
     )
-    print(test_node.props_to_html())
-    print(test_node.props)
+    print(test_node.to_html())
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,20 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 #more formal debugging
 #calls your function and compares it against a static result
 
-class TestHTMLNode(unittest.TestCase):
+#for reference, ctrl k then ctrl c to comment out
+# class HTMLNode():
+#     def __init__(self, tag=None, value=None, children=None, props=None):
+#         self.tag = tag
+#         self.value = value
+#         self.children = children
+#         self.props = props
 
+class TestHTMLNode(unittest.TestCase):
+    #HTML node tests
     def test_props_to_html_no_props(self):
         node = HTMLNode()
         self.assertEqual(node.props_to_html(), '')
@@ -19,6 +27,26 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(props={"href": "https://www.google.com", "class": "my-class"})
         self.assertEqual(node.props_to_html(), ' href="https://www.google.com" class="my-class"')
 
+    #LeafNode tests
+    #note, do not pass in children param at all, even as None
+    def test_leafnode_props(self):
+        node = LeafNode(tag="a", value="testing no props", props=None)
+        self.assertEqual(node.to_html(), '<a>testing no props</a>')
+
+    def test_leafnode_proper(self):
+        node = LeafNode(tag="a", value="testing expected input/output", props={"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">testing expected input/output</a>')
+
+    def test_leafnode_notag(self):
+        node = LeafNode(tag=None, value="No tag", props=None)
+        self.assertEqual(node.to_html(), 'No tag')
+    
+    def test_leafnode_novalue(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode(tag="p", value=None, props=None)
+            node.to_html()
+
 
 if __name__ == "__main__":
+    
     unittest.main()
